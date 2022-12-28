e84c76aac2bcaaaf57aa8bb42cd017dda6d4544a
CesarOvilla <cesarvilla903@gmail.com>
2022-12-28 14:09:34 -0600

    implementation getX and creation home page

---
+++ lib/pages/principal/principal_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PrincipalPage extends StatelessWidget {
+  const PrincipalPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Task Manager"),
+      ),
+    );
+  }
+}
