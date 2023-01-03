ca81bfe6ad9b8649cddd57cce4c45756c9e21558
CesarOvilla <cesarvilla903@gmail.com>
2023-01-02 20:59:00 -0600

    implementation of indicators

--- lib/pages/principal/body_principal.dart
+++ lib/pages/principal/body_principal.dart
@@ -4,6 +4,7 @@ import 'package:task_manager/pages/principal/task_information_alert_dialog.dart'
 import 'package:task_manager/provider/principal_provider.dart';
 import 'package:task_manager/utils/color_app.dart';
 import 'package:task_manager/widgets/card_task.dart';
+import 'package:task_manager/widgets/text_custom.dart';
 
 class BodyPrincipal extends GetView<PrincipalProvider> {
   const BodyPrincipal({
@@ -16,44 +17,94 @@ class BodyPrincipal extends GetView<PrincipalProvider> {
     return SizedBox(
       height: double.infinity,
       width: double.infinity,
-      child: Obx(
-        () => provider.tasks.value != null
-            ? ListView.builder(
-                itemCount: provider.tasks.value!.length,
-                itemBuilder: (context, index) {
-                  return CardTask(
-                    color: provider.tasks.value![index].isCompleted == 1
-                        ? ColorApp.cardTaskComplete
-                        : ColorApp.cardTaskNoComplete,
-                    taskName: provider.tasks.value![index].title,
-                    date: provider.tasks.value![index].dueDate.toString(),
-                    onTap: () {
-                      provider
-                          .getTaskId(
-                        id: provider.tasks.value![index].id!,
-                      )
-                          .then(
-                        (value) {
-                          if (value != null) {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return TaskInformationAlertDialog(
-                                  task: value,
-                                );
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _Indicators(
+                  color: ColorApp.cardTaskComplete,
+                  text: 'Completed',
+                ),
+                _Indicators(
+                  color: ColorApp.cardTaskNoComplete,
+                  text: 'Not Completed',
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Obx(
+            () => provider.tasks.value != null
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: provider.tasks.value!.length,
+                      itemBuilder: (context, index) {
+                        return CardTask(
+                          color: provider.tasks.value![index].isCompleted == 1
+                              ? ColorApp.cardTaskComplete
+                              : ColorApp.cardTaskNoComplete,
+                          taskName: provider.tasks.value![index].title,
+                          date: provider.tasks.value![index].dueDate.toString(),
+                          onTap: () {
+                            provider
+                                .getTaskId(
+                              id: provider.tasks.value![index].id!,
+                            )
+                                .then(
+                              (value) {
+                                if (value != null) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return TaskInformationAlertDialog(
+                                        task: value,
+                                      );
+                                    },
+                                  );
+                                }
                               },
                             );
-                          }
-                        },
-                      );
-                    },
-                  );
-                },
-              )
-            : const Center(
-                child: CircularProgressIndicator(),
-              ),
+                          },
+                        );
+                      },
+                    ),
+                  )
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _Indicators extends StatelessWidget {
+  const _Indicators({Key? key, required this.color, required this.text})
+      : super(key: key);
+
+  final String text;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 20,
+          width: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: TextCustom(text),
+        )
+      ],
     );
   }
 }
